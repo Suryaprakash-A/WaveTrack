82e1226470107dde22fe9f72c272d340e26d6eb9
A.Surya prakash <suryaarasu543@gmail.com>
2026-02-07 16:09:17 +0530

    Create authSlice for authentication state management

---
+++ frontend/src/redux/store/authSlice.js
@@ -0,0 +1,29 @@
+// store/authSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+// import { isTokenExpired, decodeToken } from "../../utils/jwt";
+
+const initialState = {
+  token: null,
+  user: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state, action) {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.token = null;
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
